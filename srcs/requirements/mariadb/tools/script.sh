c0e5f9f034f7cfe2e68f1bd84c0646a782946204
Mohamed El haia <moel-hai@e3r1p9.1337.ma>
2026-09-08 17:38:27 +0100

    barelly done

---
+++ srcs/requirements/mariadb/tools/script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
+MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
+
+#ERROR: 1290  The MariaDB server is running with the --skip-grant-tables option so it cannot execute this statement
+
+    mysqld --user=mysql --bootstrap << EOF
+    FLUSH PRIVILEGES;
+    CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+    CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+    GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+EOF
+
+exec mysqld --user=mysql
---
+++ srcs/requirements/wordpress/tools/wordpress_config.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
+WORDPRESS_PASSWORD=$(cat /run/secrets/wordpress_password)
+WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)
+
+mariadb-admin ping -h"mariadb" -u "${MYSQL_USER}" -p"${MYSQL_PASSWORD}" --wait=60 --silent
+
+if [ ! -f "wp-settings.php" ]; then
+    echo "installing wordpress..."
+    
+    wp core download --allow-root
+    wp config create \
+        --dbname=$MYSQL_DATABASE \
+        --dbuser=$MYSQL_USER \
+        --dbpass=$MYSQL_PASSWORD \
+        --dbhost=mariadb \
+        --allow-root
+    wp core install \
+        --url=$DOMAIN_NAME \
+        --title="Inception" \
+        --admin_user=$WORDPRESS_ADMIN_USER \
+        --admin_password=$WORDPRESS_ADMIN_PASSWORD \
+        --admin_email=$WORDPRESS_ADMIN_EMAIL \
+        --allow-root
+    wp user create $WORDPRESS_USER $WORDPRESS_EMAIL \
+        --user_pass=$WORDPRESS_PASSWORD \
+        --role=author \
+        --allow-root
+        
+    echo "wordpress is installed and ready!"
+else
+    echo "wordpress is already installed."
+fi
+
+chown -R www-data:www-data /var/www/html
+
+wp config set WP_REDIS_HOST redis --allow-root
+wp config set WP_CACHE true --raw --allow-root
+wp plugin install redis-cache --activate --allow-root
+wp redis enable --allow-root
+
+exec php-fpm8.2 -F
