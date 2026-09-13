893a24cf30a08dc582d1c8360fc06de763120ada
Mohamed El haia <moel-hai@e1r5p14.1337.ma>
2026-09-13 18:16:01 +0100

    done

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
-# MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
-
 #ERROR: 1290  The MariaDB server is running with the --skip-grant-tables option so it cannot execute this statement
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Database not found. Initializing..."
--- srcs/requirements/wordpress/tools/wordpress_config.sh
+++ srcs/requirements/wordpress/tools/wordpress_config.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
-# WORDPRESS_PASSWORD=$(cat /run/secrets/wordpress_password)
-# WORDPRESS_ADMIN_PASSWORD=$(cat /run/secrets/wordpress_admin_password)ve se
-
 cd /var/www/html
 mariadb-admin ping -h "mariadb" -u "${MYSQL_USER}" -p "${MYSQL_PASSWORD}" --wait=60 --silent
 
