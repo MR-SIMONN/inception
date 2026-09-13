fc04a8ef1d0b7da7e1fcddb3bf0326160aaaa554
Mohamed El haia <moel-hai@e1r5p14.1337.ma>
2026-09-13 16:47:03 +0100

    after adding env vars

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -4,7 +4,10 @@ MYSQL_PASSWORD=$(cat /run/secrets/mysql_password)
 MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
 
 #ERROR: 1290  The MariaDB server is running with the --skip-grant-tables option so it cannot execute this statement
-
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Database not found. Initializing..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+fi
     mysqld --user=mysql --bootstrap << EOF
     FLUSH PRIVILEGES;
     CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -13,4 +16,8 @@ MYSQL_ROOT_PASSWORD=$(cat /run/secrets/mysql_root_password)
 EOF
 
 
-exec mysqld --user=mysql
+
+
+
+
+
